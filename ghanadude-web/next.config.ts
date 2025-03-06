366051aa470f68dfe8ee6d9d654eed054dc6e703
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2025-03-07 00:36:13 +0200

    migrate

--- ghanadude-web/next.config.ts
+++ ghanadude-web/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ludmil.pythonanywhere.com",
+        hostname: "www.ghanadude.co.za",
         pathname: "/**",
       },
       {
