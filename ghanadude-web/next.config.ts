b4402747110c664d9a2ff27293f19af71c0a8485
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2025-04-16 11:17:46 +0200

    config

--- ghanadude-web/next.config.ts
+++ ghanadude-web/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.ghanadude.co.za",
+        hostname: "ucekkzhmdgmcenhvxary.supabase.co",
         pathname: "/**",
       },
       {
